ddf6271bd5a47155e2a6b64e52599a363e62ced4
Lorenzo <neppy@live.nl>
2019-10-01 19:10:35 +0200

    Added coin / points stuff

---
+++ WorkshopGame/Assets/Scripts/Lorenzo/Coin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    public event EventHandler Triggered;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<PlayerMovement>() == null)
+            return;
+        Triggered?.Invoke(this, EventArgs.Empty);                
+    }
+}
---
+++ WorkshopGame/Assets/Scripts/Lorenzo/ScoreHolder.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreHolder : MonoBehaviour
+{
+    private static int score;
+    [SerializeField]
+    private Text _text;
+
+    private void Awake()
+    {
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _text.text = score.ToString();
+    }
+    public static void AddScore(int amount) => score += amount;
+
+}
